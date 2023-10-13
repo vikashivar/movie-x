a1068dd8fb71d2088d3d45ccea5e92782fbbf39e
vikashivar <vikashivar246@gmail.com>
2023-10-13 16:59:34 +0530

    carousel  component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroBanner from './heroBanner/heroBanner'
-
+import "./style.scss";
+import Trending from './trending/Trending';
 
 const Home = () => {
   return (
     <div className='homepage' >
     
      <HeroBanner></HeroBanner>
+     <Trending></Trending>
      <div style={{height:'1000px'}}></div>
 
     </div>
