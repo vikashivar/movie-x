21114f8c1a27ad50b195114d17d8e6c4ae28cea5
vikashivar <vikashivar246@gmail.com>
2023-10-17 11:27:03 +0530

    pc

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,73 @@
-import { useState,useEffect } from 'react'
-import './App.css'
-import { useDispatch, useSelector } from 'react-redux'
-import { getApiConfiguration } from './store/homeslice'
-import Footer from './components/footer/footer'
-import Header from './components/header/header'
-import PageNotFound from './pages/404/pageNotFound'
-import Details from './pages/details/details'
-import Home from './pages/home/Home'
-import SearchResult from './pages/searchResult/SearchResult'
-import Explore from './pages/explore/Explore'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { fetchDataFromApi } from './utils/api'
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchDataFromApi } from "./utils/api";
 
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
-
-
-
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
-  const dispatch =useDispatch()
+    const dispatch = useDispatch();
+    const { url } = useSelector((state) => state.home);
+    console.log(url);
 
-  useEffect(()=>{
-    fetchApiConfig()
-  },[])
+    useEffect(() => {
+        fetchApiConfig();
+        genresCall();
+    }, []);
 
-  const fetchApiConfig=()=>{
-      fetchDataFromApi('/configuration').then((res)=>{
-        const url ={
-          backdrop:res.images.secure_base_url +'original', 
-          poster:res.images.secure_base_url +'original', 
-          profile:res.images.secure_base_url +'original', 
+    const fetchApiConfig = () => {
+        fetchDataFromApi("/configuration").then((res) => {
+            console.log(res);
 
-        }
-             dispatch(getApiConfiguration(url))
-      })
-  }
-  
-   
-  
-   
-  return (
-    <BrowserRouter>
-    <Header></Header>
-    <Routes>
-      <Route path='/' element={<Home></Home>}></Route>
-      <Route path='/:mediaType/:id' element={<Details></Details>}></Route>
-      <Route path='/search/:query' element={<SearchResult></SearchResult>}></Route>
-      <Route path='/explore/:mediaType' element={<Explore></Explore>}></Route>
-      <Route path='*' element={<PageNotFound></PageNotFound>}></Route>
+            const url = {
+                backdrop: res.images.secure_base_url + "original",
+                poster: res.images.secure_base_url + "original",
+                profile: res.images.secure_base_url + "original",
+            };
 
-    
-    </Routes>
-    <Footer></Footer>
-    </BrowserRouter>
-  )
+            dispatch(getApiConfiguration(url));
+        });
+    };
+
+    const genresCall = async () => {
+        let promises = [];
+        let endPoints = ["tv", "movie"];
+        let allGenres = {};
+
+        endPoints.forEach((url) => {
+            promises.push(fetchDataFromApi(`/genre/${url}/list`));
+        });
+
+        const data = await Promise.all(promises);
+        console.log(data);
+        data.map(({ genres }) => {
+            return genres.map((item) => (allGenres[item.id] = item));
+        });
+
+        dispatch(getGenres(allGenres));
+    };
+
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
